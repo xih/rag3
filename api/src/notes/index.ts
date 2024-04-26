d827413da938d02d91d90291056355b21aa14d3d
Dennis <dennis@momentum.local>
2024-04-25 17:38:16 -0700

    add generate_notes method

--- api/src/notes/index.ts
+++ api/src/notes/index.ts
@@ -9,6 +9,11 @@ import path from "path";
 
 import { existsSync, mkdirSync } from "fs";
 import { writeFile } from "fs/promises";
+import { PDFLoader } from "langchain/document_loaders/fs/pdf";
+import { Document } from "langchain/document";
+import { ChatOpenAI } from "langchain/chat_models/openai";
+import { NOTES_TOOL_SCHEMA, NOTE_PROMPT, noteOutputParser } from "./prompts.js";
+import { formatDocumentsAsString } from "langchain/util/document";
 
 const loadPaperFromUrl = async (url: string) => {
   const response = await axios.get(url, {
@@ -41,6 +46,34 @@ const savePaperFromUrl = async (url: string) => {
   return filePath; // return the filePath that it was saved
 };
 
+const loadPdf = async (path: string) => {
+  const loader = new PDFLoader(path);
+  const docs = await loader.load();
+
+  return docs;
+};
+
+const generateNotes = async (documents: Document[]) => {
+  const documentsAsString = formatDocumentsAsString(documents);
+
+  const model = new ChatOpenAI({
+    model: "gpt-3.5-turbo-0125",
+    temperature: 0.0,
+  });
+
+  const modelWithTool = model.bind({
+    tools: [NOTES_TOOL_SCHEMA],
+  });
+
+  const chain = NOTE_PROMPT.pipe(modelWithTool).pipe(noteOutputParser);
+
+  const response = chain.invoke({
+    paper: documentsAsString,
+  });
+
+  return response;
+};
+
 const takeNotes = async (pdfUrl: string) => {
   if (!pdfUrl.endsWith("pdf")) {
     throw new Error("not a pdf");
@@ -50,7 +83,28 @@ const takeNotes = async (pdfUrl: string) => {
   const data = await loadPaperFromUrl(pdfUrl);
   const path = await savePaperFromUrl(pdfUrl);
 
-  console.log(data, "data");
+  // 2. convert pdf to langchain documents
+  // load pdf
+  const docs = await loadPdf(path);
+
+  // 3. add newMetadata to the docs to allow the database to index by pdfURL
+  const newDocs = docs.map((doc) => {
+    return {
+      ...doc,
+      metadata: {
+        ...doc.metadata,
+        url: pdfUrl,
+      },
+    };
+  });
+
+  // 4. call generateNotes
+
+  const notes = await generateNotes(newDocs);
+  // 5. store the notes in supbase
+  // 6. return ntoes
+
+  console.log(notes, "notes");
 };
 
-takeNotes("https://arxiv.org/pdf/2305.15334.pdf");
+takeNotes("https://arxiv.org/pdf/2404.15949.pdf");
